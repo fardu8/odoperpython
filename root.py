97de8f4d0e0b7742bd133f27a46119a0ff9f7538
fardu8 <31069715+fardu8@users.noreply.github.com>
2017-08-17 20:54:47 +0530

    Update root.py

--- root.py
+++ root.py
@@ -1,13 +1,13 @@
 def roots(a,b,c,disc):
     if disc==0:
-        print("roots are real and equal:" , -b/(2*a) ," ", -b/(2*a));
+        print("roots are real and equal:" , -b/(2*a) ," ", -b/(2*a))
     elif disc>0:
-        print("roots are real:" , (-b+(disc**(1/2.0)))/(2*a) ," ", (-b-(disc**(1/2.0)))/(2*a));
+        print("roots are real:" , (-b+(disc**(1/2)))/(2*a) ," ", (-b-(disc**(1/2)))/(2*a))
     else:
-        print("roots are complex:" , (-b+(disc**(1/2.0)))/(2*a) ," ", (-b-(disc**(1/2.0)))/(2*a));
-a=float(input("enter coefficient of x^2:"));
-b=float(input("enter coefficient of x^1:"));
-c=float(input("enter the constant value:"));
+        print("roots are complex:" , (-b+(disc**(1/2)))/(2*a) ," ", (-b-(disc**(1/2)))/(2*a))
+a=float(input("enter coefficient of x^2:"))
+b=float(input("enter coefficient of x^1:"))
+c=float(input("enter the constant value:"))
 
-disc=(b*b)-(4*a*c);
-roots(a,b,c,disc);
+disc=(b*b)-(4*a*c)
+roots(a,b,c,disc)
