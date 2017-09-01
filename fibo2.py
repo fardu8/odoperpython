a75ebebcac0ca4c6e6229a9bcadee636c6f10957
fardu8 <31069715+fardu8@users.noreply.github.com>
2017-09-02 00:08:49 +0530

    Create fibo2.py

---
+++ fibo2.py
@@ -0,0 +1,26 @@
+class fibo():
+    def __init__(self):
+        self.a=self.b=0
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if c==1:
+            return self.a
+        if c==2:
+            return self.b
+        y=self.a+self.b
+        self.a=self.b
+        self.b=y
+        return y
+
+f=fibo()
+c=1
+f.a=int(input("enter the first number in the series : "))
+f.b=int(input("enter the second number in the series : "))
+n=int(input("enter the number of terms needed : "))
+x=iter(f)
+for z in x:
+    if c>n:
+        break
+    print(z)
+    c+=1        
